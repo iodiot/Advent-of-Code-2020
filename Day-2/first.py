efe9979017a8016af26dd5131948cad9e8e749df
Mike <iodiot@gmail.com>
2020-12-03 10:30:04 +0200

    Solve Dat 2

---
+++ Day-2/first.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# --- Day 2: Password Philosophy ---
+
+f = open('input.txt', 'r')
+
+valid_first, valid_second = 0, 0
+
+for line in f:
+    tokens = line.strip().split(' ')
+    fr, to = map(lambda x: int(x), tokens[0].split('-'))
+    let = tokens[1][0]
+    passwd = tokens[2]
+
+    if passwd.count(let) in range(fr, to + 1):
+        valid_first += 1
+
+    valid_second += (passwd[fr - 1] == let) ^ (passwd[to - 1] == let)
+
+print("First part: ", valid_first)
+print("Second part: ", valid_second)
+
