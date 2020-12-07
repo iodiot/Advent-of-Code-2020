6ffbf631d83d0f50a1b95e5e4f5641de01df8637
Mike <iodiot@gmail.com>
2020-12-07 10:59:18 +0200

    Solve Day 6

---
+++ Day-6/solution.py
@@ -0,0 +1,26 @@
+# --- Day 6: Custom Customs ---
+
+f = open('input.txt', 'r')
+lines = [line for line in f]
+f.close()
+
+groups = map(lambda x: x.replace('\n', ''), ''.join(lines).split('\n\n'))
+sets = map(lambda x: len(set(x)), groups)
+
+print "First part: ", sum(sets)
+
+groups = ''.join(lines).split('\n\n')
+
+count = 0
+
+for group in groups:
+    answers = group.split('\n')
+
+    s = set(answers[0])
+
+    for answer in answers:
+        s.intersection_update(answer)
+
+    count += len(s)
+
+print "Second part: ", count
---
+++ Day-8/solution.py
@@ -0,0 +1,11 @@
+#
+
+f = open('input.txt', 'r')
+
+for line in f:
+    print line
+
+f.close()
+
+print "First part: ",
+print "Second part: ",
