74cb59ec7cc5dd7ff22122b0d883f72c5acc5ce5
Mike <iodiot@gmail.com>
2020-12-08 10:21:03 +0200

    Solve Day 8

--- Day-8/solution.py
+++ Day-8/solution.py
@@ -1,11 +1,55 @@
-#
+# --- Day 8: Handheld Halting ---
 
-f = open('input.txt', 'r')
+def run(code):
+    acc, ptr = 0, 0
 
-for line in f:
-    print line
+    executed = []
+    terminates = True
 
-f.close()
+    while ptr < len(code):
+        if ptr in executed:
+            terminates = False
+            break
 
-print "First part: ",
-print "Second part: ",
+        executed.append(ptr)
+
+        op, arg = code[ptr].split(' ')
+        arg = int(arg)
+
+        if op == 'acc':
+            acc += arg
+            ptr += 1
+        elif op == 'jmp':
+            ptr += arg
+        elif op == 'nop':
+            ptr += 1
+        else:
+            raise Exception
+
+    return acc, terminates
+
+
+lines = [line.strip() for line in open('input.txt')]
+
+print "First part: ", run(lines)[0]
+
+for i in range(0, len(lines)):
+    tmp = lines[i]
+
+    op, arg = lines[i].split(' ')
+
+    new_op = op
+
+    if op == 'jmp':
+        new_op = 'nop'
+    elif op == 'nop':
+        new_op = 'jmp'
+
+    lines[i] = '{0} {1}'.format(new_op, arg)
+
+    result = run(lines)
+
+    lines[i] = tmp
+
+    if result[1]:
+        print "Second part: ", result[0]
