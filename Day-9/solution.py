ccea6279ea179aeda2e2d104d8e6f684e7e71b08
Mike <iodiot@gmail.com>
2020-12-09 10:27:15 +0200

    Solve Day 9

--- Day-9/solution.py
+++ Day-9/solution.py
@@ -1,11 +1,38 @@
-#
+# --- Day 9: Encoding Error ---
 
-f = open('input.txt', 'r')
+preamble = 25
+numbers = [int(line.strip()) for line in open('input.txt', 'r')]
 
-for line in f:
-    print line
+invalid_num = -1
 
-f.close()
+for k in range(preamble, len(numbers)):
+    seq = sorted(numbers[k - preamble:k])
 
-print "First part: ",
-print "Second part: ",
+    found = False
+
+    for i in range(0, len(seq) - 1):
+        for j in range(i + 1, len(seq)):
+            if seq[i] + seq[j] == numbers[k]:
+                found = True
+                break
+
+    if not found:
+        invalid_num = numbers[k]
+        break
+
+enc_weakness = -1
+
+for i in range(0, len(numbers) - 1):
+    if enc_weakness != -1:
+        break
+
+    for j in range(i + 1, len(numbers)):
+        seq = numbers[i:j + 1]
+        if sum(seq) == invalid_num:
+            enc_weakness = min(seq) + max(seq)
+            break
+        elif sum(seq) > invalid_num:
+            break
+
+print "First part: ", invalid_num
+print "Second part: ", enc_weakness
