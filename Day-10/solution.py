17632019ea8b77595aecf450e0f39a5859b712eb
Mike <iodiot@gmail.com>
2020-12-10 14:06:32 +0200

    Solve Day 10

--- Day-10/solution.py
+++ Day-10/solution.py
@@ -1,11 +1,23 @@
-#
+# --- Day 10: Adapter Array ---
 
-f = open('input.txt', 'r')
+ratings = sorted([int(line) for line in open('input.txt')])
 
-for line in f:
-    print line
+ratings.insert(0, 0)
+ratings.append(ratings[-1] + 3)
 
-f.close()
+differences = []
 
-print "First part: ",
-print "Second part: ",
+for i in range(1, len(ratings)):
+    differences.append(ratings[i] - ratings[i - 1])
+
+print "First part: ", differences.count(1) * differences.count(3)
+
+ratings = sorted([int(line) for line in open('input.txt')])
+ratings.append(ratings[-1] + 3)
+
+arr = {0: 1}
+
+for r in ratings:
+    arr[r] = arr.get(r - 1, 0) + arr.get(r - 2, 0) + arr.get(r - 3, 0)
+
+print "Second part: ", arr[ratings[-1]]
