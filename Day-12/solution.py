d3fed39fed4e89ab8ccf6117557b0dc5e2493e5b
Mike <iodiot@gmail.com>
2020-12-15 09:57:07 +0200

    Solve Day 12

--- Day-12/solution.py
+++ Day-12/solution.py
@@ -1,40 +1,32 @@
 # --- Day 12: Rain Risk ---
 
-DIRS = 'ESWN'
-VECTORS = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+DIRS = {'E': 1 + 0j, 'S': 0 - 1j, 'W': -1 + 0j, 'N': 0 + 1j}
 
 actions = [line.strip() for line in open('input.txt')]
-actions = map(lambda x: (x[0], int(x[1:])), actions)
+actions = list(map(lambda x: (x[0], int(x[1:])), actions))
 
-print actions
-
-d, x, y = 0, 0, 0
+ship_1 = 0 + 0j
+ship_2 = 0 + 0j
+waypoint = 10 + 1j
+dir = DIRS['E']
 
 for action in actions:
     let, step = action
 
     if let in DIRS:
-        x += VECTORS[DIRS.index(let)][0] * step
-        y += VECTORS[DIRS.index(let)][1] * step
+        ship_1 += DIRS[let] * step
+        waypoint += DIRS[let] * step
     elif let == 'F':
-        x += VECTORS[d][0] * step
-        y += VECTORS[d][1] * step
+        ship_1 += dir * step
+        ship_2 += waypoint * step
     elif let == 'L':
-        z = d
-        d = (d - step // 90)
-        if d < 0:
-            d += 4
-        print action, DIRS[z], '->', DIRS[d]
+        dir *= complex(1j) ** (step // 90)
+        waypoint *= (complex(1j) ** (step // 90))
     elif let == 'R':
-        z = d
-        d = (d + step // 90) % 4
-        print action, DIRS[z], '->', DIRS[d]
+        dir *= complex(-1j) ** (step // 90)
+        waypoint *= (complex(-1j) ** (step // 90))
     else:
         raise Exception
 
-    #print action, x, y
-
-print ("First part: ", abs(x + y))
-print ("Second part: ", 0)
-
-# 1199 -- 1400
+print("First part: ", round(abs(ship_1.imag) + abs(ship_1.real)))
+print("Second part: ", round(abs(ship_2.imag) + abs(ship_2.real)))
