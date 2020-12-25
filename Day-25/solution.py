a8a3f5d04994bd4d003a5ff4372095c87d9cf402
Mike <iodiot@gmail.com>
2020-12-25 09:48:31 +0200

    Solve Day 25

--- Day-25/solution.py
+++ Day-25/solution.py
@@ -1,8 +1,21 @@
-#
+# --- Day 25: Combo Breaker ---
 
-lines = [line.strip() for line in open('input.txt')]
+def calc_loop_size(public_key):
+    loops, n = 1, 1
+    while True:
+        n = (n * 7) % 20201227
+        if n == public_key:
+            break
+        loops += 1
+    return loops
 
-print(lines)
 
-print("First part: ", )
-print("Second part: ", )
+card_public, door_public = [int(line) for line in open('input.txt')]
+
+card = 1
+
+for i in range(0, calc_loop_size(door_public)):
+    card = (card * card_public) % 20201227
+
+print("Final answer: ", card)
+
